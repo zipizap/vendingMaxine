dae515202b35d954f496cb2282c9f9ed559c4ff5
zipizap <zipizap123@gmail.com>
2025-03-28 12:09:13 +0100

    wip

---
+++ reactweb/src/Collections.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Collections = () => {
+  return (
+    <div>
+      <h1>Collections</h1>
+      <p>This is the collections webpage.</p>
+    </div>
+  );
+};
+
+export default Collections;
---
+++ reactweb/src/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; // Import the main App.css for styling
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>VendingMaxine</h1>
+      <p className="home-text">Inspired by chillwave 80's style</p>
+    </div>
+  );
+};
+
+export default Home;
